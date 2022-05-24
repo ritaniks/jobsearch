6c27d346ba474c9edfb0928a8257782e8baa303e
Kristina <kristy.tabak@gmail.com>
2022-05-24 02:55:20 +0200

    Init Setup

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+
+import Job from "./components/Job";
+import Nav from "./components/Nav";
+import OrderBy from "./components/OrderBy";
+import OrderContext from "./contexts/OrderContext";
+import { OrderTypes } from "./types/order";
+import JobDefinition from "./types/job";
+
+import "./App.css";
+
+const App: React.FC = () => {
+  const [jobs, setJobs] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData: () => Promise<void> = async () => {
+    const result = await fetch("/jobs.json");
+    const data = await result.json();
+    setLoading(false);
+    setJobs(data);
+  };
+
+  let order: Function;
+
+  const toggleOrder = async (newOrder: string) => {
+    order(newOrder);
+  };
+
+  order = (f: string) => {
+    console.log(f);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => fetchData(), 1000);
+  }, []);
+
+  const sortedJobs: JobDefinition[] = [...jobs].sort(
+    (pr1, pr2) => pr1.priority - pr2.priority
+  );
+  console.log("test:", sortedJobs);
+
+  const JobList: React.ReactElement[] = sortedJobs.map((value) => {
+    const { id } = value;
+    return <Job key={id} {...value} />;
+  });
+
+  const loaderStyle: React.CSSProperties = {
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  return (
+    <div className="App">
+      <Nav />
+      {loading && (
+        <div style={loaderStyle}>
+          <ClimbingBoxLoader color={"#00c"} loading={loading} size={15} />
+        </div>
+      )}
+      <OrderContext.Provider
+        value={{
+          orderby: OrderTypes.Random,
+          toggleOrder,
+        }}
+      >
+        {!!JobList.length && (
+          <div data-testid="app-jobs" className="App-jobs">
+            <OrderBy />
+            {JobList}
+          </div>
+        )}
+      </OrderContext.Provider>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/OrderBy/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useContext } from "react";
+
+import { OrderTypes } from "../../types/order";
+import OrderContext from "../../contexts/OrderContext";
+import "./index.css";
+
+const OrderBy: React.FC = () => {
+  const [selected, setSelected] = useState(OrderTypes.Random);
+  const { toggleOrder } = useContext(OrderContext);
+  toggleOrder?.(selected);
+
+  const onOrderChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
+    if (
+      e.target.value === OrderTypes.Random ||
+      e.target.value === OrderTypes.Prioprity
+    ) {
+      setSelected(e.target.value);
+    }
+  };
+
+  return (
+    <div className="App-sortby">
+      <span className="App-sortby__label">Sort by: </span>
+      <select
+        className="App-sortby__select"
+        id="filtersortby"
+        onChange={onOrderChange}
+        value={selected}
+      >
+        <option value={OrderTypes.Random}>Random</option>
+        <option value={OrderTypes.Prioprity}>By Priority</option>
+      </select>
+    </div>
+  );
+};
+
+export default OrderBy;
